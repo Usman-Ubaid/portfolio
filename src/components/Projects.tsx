6e21ad559403b738fc152645ae9ea7e536765a08
Welcome ! <usmanubaid9@gmail.com>
2023-11-25 22:47:09 +0100

    refactor projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import ProjectCard from "./projectSection/ProjectCard";
+
 const Projects = () => {
   return (
     <section id="projects" className="projects">
@@ -5,48 +7,7 @@ const Projects = () => {
         My <span>Projects</span>
       </h2>
       <div className="projects-wrapper">
-        <div className="project-box">
-          <img src="./images/weatherApp.jpg" alt="Weather-Pic" />
-          <div className="project-content">
-            <h3>Weather App</h3>
-            <p>
-              A ReactJS frontend web app dynamically displaying weather
-              information.
-            </p>
-            <a
-              href="https://weather-app-two-chi-73.vercel.app/"
-              target="_blank"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-        <div className="project-box">
-          <img src="./images/taskApp.jpg" alt="Task-Tracker-Pic" />
-          <div className="project-content">
-            <h3>Task Tracker App</h3>
-            <p>A ReactJS To-Do Project that keeps track of tasks.</p>
-            <a
-              href="https://task-tracker-rouge-three.vercel.app/"
-              target="_blank"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-        <div className="project-box">
-          <img src="./images/cartApp.jpg" alt="Shopping-Cart-Pic" />
-          <div className="project-content">
-            <h3>Shopping Cart App</h3>
-            <p>A React-TS shopping cart frontend web app.</p>
-            <a
-              href="https://shopping-cart-gamma-jet.vercel.app/"
-              target="_blank"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
+        <ProjectCard />
       </div>
     </section>
   );
