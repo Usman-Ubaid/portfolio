1b02b151b2b44fa431be047650aae10cbb72b051
Welcome ! <usmanubaid9@gmail.com>
2023-11-26 17:01:57 +0100

    slide in navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,45 @@
+import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
+  const handleNavItem = () => {
+    setToggle(!toggle);
+  };
+
+  const navItems = [
+    { id: 1, navItem: "Home", path: "#home" },
+    { id: 2, navItem: "Projects", path: "#projects" },
+    { id: 3, navItem: "Skills", path: "#skills" },
+    { id: 4, navItem: "Education", path: "#education" },
+  ];
+
   return (
     <header className="header">
       <div className="menu-logo">
         <h2>
           <a href="#">Usman.</a>
         </h2>
-        <FiMenu className="bx bx-menu" />
+        <div
+          onClick={handleClick}
+          className={`menu-bar ${toggle ? "active" : ""}`}
+        >
+          <FiMenu />
+        </div>
       </div>
 
-      <nav className="primary-nav">
-        <ul className="nav-list">
-          <li>
-            <a className="active" href="#home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#education">Education</a>
-          </li>
+      <nav className={`primary-nav ${toggle ? "active" : ""}`}>
+        <ul className={`nav-list ${toggle ? "active" : ""}`}>
+          {navItems.map((item) => (
+            <li key={item.id} onClick={handleNavItem}>
+              <a href={item.path}>{item.navItem}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
